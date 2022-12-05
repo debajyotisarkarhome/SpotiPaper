9dd4792492959138fe8241f293bb6cc425e43a2b
Debajyoti sarkar <42748272+debajyotisarkarhome@users.noreply.github.com>
2022-12-05 20:21:16 +0000

    added API calls

--- main.py
+++ main.py
@@ -5,21 +5,34 @@ from flask import Flask, flash, request, redirect, url_for, render_template, sen
 from werkzeug.utils import secure_filename
 import pandas as pd
 import uuid
+import json
 
 app = Flask(__name__)
 
+token = os.environ['OAuth']
 
 
 @app.route("/")
 def uploader_file():
   return render_template("index.html")
 
+
 @app.route("/create")
 def createAndSend():
-  return 0
+  args = request.args
+  playlist_link = args.get("playlist")
+  device = args.get("device")
+  headers = {"Accept": "application/json", "Authorization": "Bearer " + token}
+  res = json.loads(
+    requests.get("https://api.spotify.com/v1/playlists/" + playlist_link +
+                 "/tracks",
+                 headers=headers).text)
+  for i in range(0, len(res["items"])):
+    imagelist = res["items"][i]["track"]["album"]["images"]
 
+    print(imagelink)
+  return "1"
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8080)
-  
+  app.run(host='0.0.0.0', port=8080)
